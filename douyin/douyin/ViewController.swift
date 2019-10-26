6595f5dcbc5b08d6c34b27a3b4d14e26a32411cf
xbk <xiongbk@edutech.com.cn>
2019-10-26 21:44:27 +0800

    项目初始化

---
+++ douyin/douyin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  douyin
+//
+//  Created by bk.xiong on 2019/10/26.
+//  Copyright © 2019 xiongbk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
